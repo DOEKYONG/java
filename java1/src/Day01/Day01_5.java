184b9a37b59015793d2ac8a732476a96458e8d76
504 <504@DESKTOP-K2AN7ON>
2022-02-28 17:54:46 +0900

    1234

--- java1/src/Day01/Day01_5.java
+++ java1/src/Day01/Day01_5.java
@@ -24,9 +24,10 @@ public class Day01_5 { // class start
 	System.out.print("내용: "); String contents = scanner1.next();
 	System.out.print("날짜: "); String date = scanner1.next();
 	
-	System.out.println("* ------------방문록------------|");
-	System.out.println("*|순번|작성자|내용\t   |날짜  |");
-	System.out.println("*|"+num+"  |" +user+"|"+contents+"  |"+date+"|");
+	System.out.println("*------------방문록------------|");
+	System.out.println("*|순번|작성자|내용\t     |날짜  |");
+	System.out.println("*|"+num+"  |" +user+"|"+contents+"   |"+date+"|");
+	System.out.println("* -----------------------------|");
 	
 	
 	} // main end
