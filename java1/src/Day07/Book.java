45197ec3980e2e3a7c351bf948b82536ab1bd9da
apoch <apoch@192.168.123.100>
2022-03-10 21:36:53 +0900

    도서반납 실행x

--- java1/src/Day07/Book.java
+++ java1/src/Day07/Book.java
@@ -1,113 +1,130 @@
 package Day07;
 
+import java.util.Scanner;
+
 public class Book {
 	
 	// 1. 필드
-		String ISBN; // ISBN [ 도서번호 = 식별용 ]
-		String bname; // 도서명
-		String bwriter; // 작가
-		boolean brental; // 도서대여여부
-		String mid; // 대여인ID
-	
-	
+	String ISBN;	// ISBN [ 도서번호 = 식별용 ] 
+	String bname;	// 도서명
+	String bwriter;	// 작가
+	boolean brental;// 도서대여여부
+	String mid;		// 대여인ID
 	
 	// 2. 생성자
-		// 1. 빈생성자[기본생성자] : 메소드호출용
-		
-		public Book() {}
+		// 1. 빈생성자[기본생성자] : 메소드호출용   ( ctrl+스페이바)
+	public Book() {}
 		// 2. 풀생성자[모든필드받는생성자] : 도서등록
-		
-		public Book(String ISBN, String bname, String bWriter, boolean brental, String mid) {
-			this.ISBN = ISBN;
-			this.bname = bname;
-			this.bwriter = bWriter;
-			this.brental = brental;
-			this.mid = mid;
-		}
-		
-		
-		
-		
-	
-	// 3. 메소드
-		// 1. 도서검색
-		void 도서검색() {
-			
-		}
-		// 2. 도서목록
-		void 도서목록() {
-			System.out.println("------------도서목록페이지------------------");
-			System.out.println("ISBN\t도서명\t작가\t대여가능여부");
-			for( Book temp : Day07_5_BookApplication.books) {
-				if( temp != null ) { // 도서가 존재하면 
-					if(  temp.brental ) // 해당도서의 도서대여여부가 true 이면 
-						System.out.println( temp.ISBN +"\t"+ temp.bname +"\t" + temp.bwriter +"\t" + "대여가능");
-					else // 해당도서의 도서대여여부가 false 
-						System.out.println( temp.ISBN +"\t"+ temp.bname +"\t" + temp.bwriter +"\t" + "대여중");
-				}
+	public Book(String iSBN, String bname, String bwriter, boolean brental, String mid) {
+		ISBN = iSBN;
+		this.bname = bname;
+		this.bwriter = bwriter;
+		this.brental = brental;
+		this.mid = mid;
+	}
+	// 3. 메소드 
+		// 1. 도서검색 메소드
+	void 도서검색() { 
+		System.out.println(" ------- 도서검색 페이지 -------");
+	}
+		// 2. 도서목록 메소드
+	void 도서목록() {
+		System.out.println(" ------- 도서목록 페이지 -------");
+		System.out.println("ISBN\t도서명\t작가\t대여가능여부");
+		for( Book temp : Day07_5_BookApplication.books) {
+			if( temp != null ) { // 도서가 존재하면 
+				if(  temp.brental ) // 해당도서의 도서대여여부가 true 이면 
+					System.out.println( temp.ISBN +"\t"+ temp.bname +"\t" + temp.bwriter +"\t" + "대여가능");
+				else // 해당도서의 도서대여여부가 false 
+					System.out.println( temp.ISBN +"\t"+ temp.bname +"\t" + temp.bwriter +"\t" + "대여중");
 			}
 		}
-//		// 3. 도서대여
-//		void 도서대여(String loginid) {
-//			System.out.println("------------도서대여페이지------------------");
-//			System.out.println(" 도서 ISBN : "); String isbn = Day07_5_BookApplication.scanner.next();
-//			for ( Book temp : Day07_5_BookApplication.books) {
-//				
-//				if ( temp.ISBN.equals(isbn) && temp.brental) {
-//				
-//					if(temp.brental) {
-//						System.out.println("해당 도서 대여 합니다..");
-//						temp.brental = false; // 대여중으로변경
-//						temp.mid = loginid;
-//						return;
-//					}else {
-//						System.out.println("해당도서 대여중 상태입니다.");
-//						
-//						return;
-//					}
-//				}
-//			}
-//			System.out.println("동일한 도서 ISBN이 없습니다. 대여불가");
-//			
-//		}
-//		// 4. 도서반납
-//		void 도서반납 (String loginid) {
-//			System.out.println("---------도서반납페이지-------------");
-//			도서대여목록(loginid);
-//			
-//		}
-		// 5. 도서등록
-		void 도서등록 () {
-			System.out.println("------------도서등록페이지------------------");
-			System.out.println("도서 ISBN : "); String ISBN = Day07_5_BookApplication.scanner.next();
-			// 도서 고유번호 중복체크
-			for ( Book temp : Day07_5_BookApplication.books) {
-				if(temp != null) {
-				if(  temp.ISBN.equals(ISBN)) {
-				System.out.println("이미 등록된 고유식별번호입니다.");
-				return;
-				}
-				}
-			}
-			System.out.println("도서 제목 : "); String bname = Day07_5_BookApplication.scanner.next();
-			System.out.println("도서 작가 : "); String bwriter = Day07_5_BookApplication.scanner.next();
-			
-			Book book = new Book(ISBN,bname,bwriter,true,null);
-			
-			int i = 0 ;
-			for ( Book temp : Day07_5_BookApplication.books) {
-				if(temp == null) {
-					Day07_5_BookApplication.books[i] = book;
-					System.out.println("도서등록완료!!!");
+	}
+		// 3. 도서대여 메소드
+	void 도서대여( String loginid ) {
+		System.out.println(" ------- 도서대여 페이지 -------");
+		System.out.println(" 도서 ISBN : ");	String isbn = Day07_5_BookApplication.scanner.next();
+		
+		for( Book temp : Day07_5_BookApplication.books ) {
+			if( temp != null && temp.ISBN.equals(isbn) ) {
+				if( temp.brental ) {
+					System.out.println(" 알림]] 해당 도서 대여 합니다.");
+					temp.brental = false; // 대여중으로 변경 
+					temp.mid =loginid; // 현재 로그인한 id를 대입
+					return;
+				}else {
+					System.out.println(" 알림]] 해당 도서 대여중 상태 입니다 . [ 대여불가 ] ");
 					return;
 				}
-				i++;
 			}
-			
-			
 		}
-		// 6. 도서삭제
-		void 도서삭제() {
-			
+		System.out.println(" 알림]] 동일한 도서ISBN이 없습니다. ");
+	}
+		// 4. 도서반납 메소드
+	void 도서반납( String loginid ) {
+		System.out.println(" ------- 도서반납 페이지 -------");
+		도서대여목록( loginid  );
+		System.out.println(" 도서 ISBN "); String isbn = Day07_5_BookApplication.scanner.next();
+		for( Book temp : Day07_5_BookApplication.books ) {
+			if( temp !=null && temp.ISBN.equals(isbn) ) { // 입력한 isbn이 있으면
+				if( temp.mid.equals(loginid) ) { // 대여인id 과 현재 로그인된id 동일하면 
+					if( temp.brental ) { // 대여중이 아니면 
+						System.out.println(" 알림]] 현재 도서가 대여중이 아닙니다. ");
+					}else { // 대여중이면 
+						System.out.println(" 알림]] 반납 완료 !!!! ");
+						temp.brental = true; // 대여중 -> 대여가능 변경 
+						temp.mid = null; // 대여한 사람의 id를 다시 없음변경 [ null ]
+						return;
+					}
+				}else {
+					System.out.println(" 알림]] 회원님이 대여한 도서가 아닙니다.");
+				}
+			}
 		}
+		System.out.println(" 알림]] 동일한 도서ISBN이 없습니다. ");
+	}
+		// * 현재 로그인 한 회원이 대여중인 도서 목록 [ 내 도서대여 목록 ] 
+	void 도서대여목록( String loginid ) {
+		System.out.println(" ------- 도서목록 페이지 -------");
+		System.out.println("ISBN\t도서명\t작가\t대여가능여부");
+		for( Book temp : Day07_5_BookApplication.books) {
+			if( temp != null && temp.mid.equals( loginid ) ) {
+				// * 도서가 존재하면서 대여id 와 현재 로그인된id와 같으면
+				System.out.println( temp.ISBN +"\t"+ temp.bname +"\t" + temp.bwriter +"\t" + "대여중");
+			}
+		}
+	}
+	
+		// 5. 도서등록 메소드
+	void 도서등록() {
+		System.out.println(" ------- 도서등록 페이지 -------");
+		// 1. 입력받아 
+		System.out.print(" ISBN : ");	String isbn = Day07_5_BookApplication.scanner.next();
+		System.out.print(" 도서명 : ");	String bname = Day07_5_BookApplication.scanner.next();
+		System.out.print(" 작가 : ");	String bwriter = Day07_5_BookApplication.scanner.next();
+		//*중복체크[isbn]
+		for( Book temp : Day07_5_BookApplication.books ) {
+			if(  temp !=null && temp.ISBN.equals(isbn) ) {
+				System.out.println(" 알림]] 현재 사용중인 ISBN 입니다. [ 등록실패 ]");
+				return; // 도서등록 메소드 종료 ( 등록실패 )
+			}
+		}
+		// 2. 객체화 [ 도서대여여부 = true , 대여회원id = null ]
+		Book book = new Book(isbn, bname, bwriter, true , null);
+		// 3. 배열 대입[넣기]
+		int i = 0; // 반복횟수 [ 인덱스 ]
+		for( Book temp : Day07_5_BookApplication.books ) {
+			if(temp == null ) {
+				Day07_5_BookApplication.books[i]=book;
+				System.out.println(" 알림]] 도서등록이 되었습니다. ");
+				return; // 도서등록 메소드 종료 ( 등록성공 )
+			}
+			i++; // 인덱스증가
+		}
+	}
+		// 6. 도서삭제 메소드
+	void 도서삭제() {
+		System.out.println(" ------- 도서삭제 페이지 -------");
+	}
+	
 }
--- java1/src/Day07/Day07_5_BookApplication.java
+++ java1/src/Day07/Day07_5_BookApplication.java
@@ -21,15 +21,14 @@ public class Day07_5_BookApplication {
 			// 2. 일반회원메뉴 [ 1.도서검색 2.도서목록 3.도서대여 4.도서반납 5.로그아웃 ]
 			// 3. 관리자메뉴  [ 1.도서등록 2.도서목록 3.도서삭제 4.순위(도전) 5.로그아웃
 	
-	
 	// * main 밖에 변수를 선언하는 이유 : 모든 메소드에서 사용하기 위해서 
 		// 지역변수  :  { 변수선언 } 괄호내에서 선언된 변수는 { } 밖을 못나감
 			// 서로 다른 { } 괄호 내에서 사용시 -> 전역변수 
 		// static  : 프로그램 시작시 메모리 할당 / 프로그램 종료시 메모리 초기화
 			// 프로그램 전반적으로 모든곳에 사용되는 메모리[변수]
 	
-	static Scanner scanner = new Scanner(System.in); // static : 모든곳에서 사용가능
-	static Member[] members = new Member[1000]; // 모든 파일에서 사용하는 회원목록 배열
+	static Scanner scanner = new Scanner(System.in); // static : 모든곳에서 사용가능 
+	static Member[] members = new Member[1000]; // 모든 클래스에서 사용하는 회원목록 배열
 	static Book[] books = new Book[1000];
 	
 	// 0. 코드를 읽어주는 시작 메소드 [ 스레드 ]
@@ -51,32 +50,30 @@ public class Day07_5_BookApplication {
 			System.out.println("-------------- 도서 대여 프로그램 -------------");
 			System.out.print("1.회원가입 2.로그인 3.아이디찾기 4.비밀번호찾기 : ");
 			int ch = scanner.nextInt();
-			
 			Member member = new Member(); // 1~4 {}괄호에서 사용 가능
 			if( ch == 1 ) {
-				boolean result = member.회원가입();// 회원가입 메소드 호출 
-				if( result ) System.out.println("회원가입성공!!");
-				else System.out.println("회원가입실패 ㅜㅜ");
 				
-			
+				boolean result =  member.회원가입();// 회원가입 메소드 호출 
+				if( result ) 
+					System.err.println(" 알림]] 회원가입 성공 ");
+				else 
+					System.err.println(" 알림]] 회원가입 실패");
+				
 			} 
 			else if( ch ==2 ) {
 				String result = member.로그인(); // 로그인 메소드 호출 
 				
-				if ( result == null) {
-					System.out.println("동일한 회원정보가 없습니다.");
-				}
-				else if( result.equals("admin")) {
+				if( result == null ) { // 로그인실패 
+					System.out.println(" 알림]] 동일한 회원정보가 없습니다.");
+				}else if( result.equals("admin")) { // 관리자
 					// 2. 관리자 메뉴 메소드 호출 
-					System.out.println("안녕하세요~~" +result+"님");
-					adminmenu();
-				}else {
-					// 1. 일반회원 메뉴 메소드 호출 
-					System.out.println("안녕하세요~~" +result+"님");
-					membermenu(result);
+					System.out.println(" 알림]] 안녕하세요~ "+result+"님");
+					adminmenu(); // 내부 메소드 호출
+				}else{ // 일반 
+					// 1. 일반회원 메뉴 메소드 호출
+					System.out.println(" 알림]] 안녕하세요~ "+result+"님");
+					membermenu( result ); // 인수1개 [ 로그인 성공한 id ] 
 				}
-				
-					
 			}
 			else if( ch ==3 ) {
 				member.아이디찾기();// 아이디 찾기 메소드 호출 
@@ -84,55 +81,38 @@ public class Day07_5_BookApplication {
 			else if( ch ==4 ) {
 				member.비밀번호찾기(); // 비밀번호 찾기 메소드 소출 
 			}
-			else {
-				System.err.println(" 알림]] 알수 없는 번호입니다. ");
-			} 
-		} // while end
+			else { System.err.println(" 알림]] 알수 없는 번호입니다. "); } 
+		}
 	} // menu end 
 	
 	// 2. 일반회원 메뉴 메소드
-	void membermenu(String loginid) {
-		System.out.println("---------------회원메뉴------------------");
-		System.out.println("1.도서검색 2.도서목록 3.도서대여 4.도서반납 5.로그아웃");
-		int ch = scanner.nextInt();
-		Book book = new Book();
-		if(ch==1) {
-			book.도서검색();
+	void membermenu( String loginid ) { // 인수 변수명은 달라도 되지만 자료형 동일 
+		while(true) {
+			System.out.println(" --------------- 회원 메뉴 ------------------");
+			System.out.print("1.도서검색 2.도서목록 3.도서대여 4.도서반납 5.로그아웃 선택 : ");
+			int ch = scanner.nextInt();
+				Book book = new Book(); // 빈생성자로 객체 => 메소드 호출용
+			if( ch == 1 ) 		{ book.도서검색(); }
+			else if( ch ==2 ) 	{ book.도서목록(); }
+			else if( ch == 3 ) 	{ book.도서대여( loginid ); }
+			else if( ch == 4 ) 	{ book.도서반납( loginid ); }
+			else if( ch == 5 ) 	{ return; } // 일반회원메뉴 메소드 종료 [ 로그아웃 ] 
+			else { System.err.println(" 알림]] 알수 없는 번호입니다. "); }
 		}
-		else if(ch==2) {
-			book.도서목록();
-		}
-		else if(ch==3) {
-			book.도서대여(loginid);
-		}
-		else if(ch==4) {
-			book.도서반납(loginid);
-		}
-		else if(ch==5) {
-			return;
-		}
-		else {System.out.println("알수없는 번호입니다.");}
-		
-	}
+	} // 일반회원 메뉴 종료 
 	// 3. 관리자 메뉴 메소드 
 	void adminmenu() {
-		System.out.println("---------------관리자메뉴------------------");
-		System.out.println("1.도서등록 2.도서목록 3.도서삭제 4.로그아웃");
-		int ch = scanner.nextInt();
-		Book book = new Book();
-		if(ch==1) {
-			book.도서등록();
+		while(true) {
+			System.out.println(" --------------- 관리자 메뉴 ------------------");
+			System.out.print("1.도서등록 2.도서목록 3.도서삭제 4.로그아웃 선택 : ");
+			int ch = scanner.nextInt();
+				Book book = new Book();
+			if( ch == 1 ) 		{ book.도서등록(); }
+			else if( ch == 2 ) 	{ book.도서목록(); }
+			else if( ch == 3 ) 	{ book.도서삭제(); }
+			else if( ch == 4 ) 	{ return; }		// 관리자메뉴 메소드 종료 
+			else { System.err.println(" 알림]] 알수 없는 번호입니다. "); }
 		}
-		else if(ch==2) {
-			book.도서목록();
-		}
-		else if(ch==3) {
-			book.도서삭제();
-		}
-		else if(ch==4) {
-			return;
-		}
-		else {System.out.println("알수없는 번호입니다.");}
 	}
 	
 	
