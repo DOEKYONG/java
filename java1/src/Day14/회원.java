750b3002ead164e9f67122488da26520b6e096bb
504 <504@DESKTOP-HTGBDLE>
2022-03-21 18:25:56 +0900

    Day14

---
+++ java1/src/Day14/회원.java
@@ -0,0 +1,16 @@
+package Day14;
+
+import java.util.ArrayList;
+
+public class 회원 {
+	
+	String id;
+	String password;
+	String name;
+	int point;
+	private 게시판 게시판 = new 게시판();
+	private ArrayList<String> list = new ArrayList<>();
+	private 게시판[] 게시물목록 = new 게시판[100];
+	
+
+}
